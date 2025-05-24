aed1e6c8d0d48c8fca2d110c4ad576d64bce90cb
Abdul-Saidat <yewandeabdul822@gmail.com>
2025-05-24 16:32:52 +0100

    added modal stylesheet

--- main.js
+++ main.js
@@ -13,31 +13,17 @@ const images = document.querySelectorAll("img");
 images.forEach((img) => {
   img.addEventListener("click", function () {
     const imageContainer = document.createElement("div"); // create and style div element
-    imageContainer.style.top = "0";
-    imageContainer.style.left = "0";
-    imageContainer.style.width = "100%";
-    imageContainer.style.height = "100%";
-    imageContainer.style.backgroundColor = "rgba(0, 0, 0, 0.8)";
-    imageContainer.style.display = "flex";
-    imageContainer.style.position = "fixed";
-    imageContainer.style.justifyContent = "center";
-    imageContainer.style.alignItems = "center";
-    imageContainer.style.zIndex = "1000";
+    imageContainer.className = 'img-preview-styles'
     imageContainer.innerHTML = "";
 
     // create and  style image element
     const previewImage = document.createElement("img");
     previewImage.src = img.src;
-    previewImage.style.maxWidth = "80%";
-    previewImage.style.maxHeight = "80%";
 
     // create and style p element
     const imgTitle = document.createElement("p");
+    imgTitle.className = 'image-name'
     imgTitle.textContent = img.getAttribute("data-name");
-    imgTitle.style.color = "#fff";
-    imgTitle.style.fontSize = "20px";
-    imgTitle.style.marginTop = "0px";
-    imgTitle.style.marginInline = "10px";
 
     // append the image and p elements to the div
     imageContainer.appendChild(previewImage);
