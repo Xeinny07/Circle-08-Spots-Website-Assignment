473844b448980e6e27384753b8c7c039831cd0fd
Olawale Akinbileje Emmanuel <akinbilejeemmanuel@gmail.com>
2025-05-25 15:08:20 +0100

    i updated the edit profile modal, and added some animation and also made some correction to the image preview function

---
+++ components/js/animation.js
@@ -0,0 +1,7 @@
+window.addEventListener("DOMContentLoaded", function () {
+  document.querySelector(".avatar").style.opacity = "0";
+  document.querySelector(".avatar-name").style.opacity = "0";
+  document.querySelector(".avatar-field").style.opacity = "0";
+  document.querySelector(".edit-profile").style.opacity = "0";
+  document.querySelector(".button-large").style.opacity = "0";
+});
